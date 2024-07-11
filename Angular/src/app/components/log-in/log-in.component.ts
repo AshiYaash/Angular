41c790c15ea120ef7f11889480d47959c64a6f83
Deepak28102000 <deepakrajsandeep@gmail.com>
2024-07-11 17:59:15 +0530

    fifth commit

--- Angular/src/app/components/log-in/log-in.component.ts
+++ Angular/src/app/components/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -6,6 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './log-in.component.css'
 })
 export class LogInComponent {
+
+  constructor(
+    private router:Router,
+  ){}
+  form = new FormGroup({
+    "firstName": new FormControl("", Validators.required),
+    "password": new FormControl("", Validators.required),
+
+});
+onSubmit() {
+  console.log("reactive form submitted");
+  console.log(this.form);
+  }
+  login() {
+    console.log(this.form);
+    alert("Hello! I am an alert box!");
+    this.router.navigate(['/signup'])
+  }
+  
+
 hide: any;
 
 
